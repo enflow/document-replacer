fc2e4953f92f11a75f75605c3c5902c313c9aaff
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2021-12-04 20:17:39 +0100

    Tweak test bash script

--- tests.sh
+++ tests.sh
@@ -1,10 +1,24 @@
-trap cleanup INT TERM
+killall soffice.bin
+killall unoserver
 
 cleanup() {
     killall unoserver
+    exit
 }
+trap cleanup INT TERM
 
-unoserver --daemon
-sleep 1
+unoserver &
 
+echo -e "Waiting for unoserver to start..."
+
+while ! nc -z 127.0.0.1 2002; do
+  echo -n "."
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+echo -e "Running tests"
 vendor/bin/phpunit
+
+killall unoserver
+
+exit $?
