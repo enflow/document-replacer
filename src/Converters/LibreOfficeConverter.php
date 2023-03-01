a8b8940f1897bf489f06ee66915bcf37a8e7e7a1
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2023-03-01 21:56:56 +0100

    Initial pass at adding a LibreOffice converter

---
+++ src/Converters/LibreOfficeConverter.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Enflow\DocumentReplacer\Converters;
+
+use Enflow\DocumentReplacer\Exceptions\ConversionFailed;
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
+
+class LibreOfficeConverter extends AbstractConverter
+{
+    private const BINARY = '/usr/bin/libreoffice';
+
+    protected array $libreOfficeOptions = [
+        '--headless',
+        '--invisible',
+        '--nocrashreport',
+        '--nodefault',
+        '--nofirststartwizard',
+        '--nologo',
+        '--norestore',
+    ];
+
+    public function convert(string $input, string $output): void
+    {
+        $temporaryOutputDirectory = sys_get_temp_dir() . '/document-replacer-' . uniqid() . '-libreoffice';
+
+        try {
+            $binary = $this->options['binary'] ?? (new ExecutableFinder())->find('libreoffice') ?? static::BINARY;
+
+            $process = Process::fromShellCommandline(implode(' ', [
+                $binary,
+                '--convert-to pdf:writer_pdf_Export',
+                ...$this->libreOfficeOptions,
+                '--outdir ' . escapeshellarg($temporaryOutputDirectory),
+                escapeshellarg($input),
+            ]));
+            $process->setInput($input);
+            $process->setTimeout(20);
+            $process->mustRun();
+
+            if (! file_exists($temporaryOutputDirectory)) {
+                throw new ConversionFailed('Failed to convert document: no output directory was created');
+            }
+
+            $files = glob($temporaryOutputDirectory . "/*.pdf");
+            if (count($files) !== 1) {
+                throw new ConversionFailed('Failed to convert document: more than one file was created');
+            }
+
+            rename($files[0], $output);
+        } finally {
+            if (file_exists($temporaryOutputDirectory)) {
+                rmdir($temporaryOutputDirectory);
+            }
+        }
+    }
+}
